15b41132ffe9ec056f17be1ba82c4a34a8976f43
Damien PIQUET <piqudam@gmail.com>
2025-05-26 10:43:58 +0200

    some refactoring

--- git-clicker/src/main.tsx
+++ git-clicker/src/main.tsx
@@ -1,76 +1,15 @@
 import {createRoot} from 'react-dom/client'
-import {useEffect} from "react";
 
-import items from "./items.json";
-import {Provider, useDispatch, useSelector} from "react-redux";
-import store, {RootState} from "./store.ts";
-import {buyItem, incrementScore, Item} from "./modules/game";
+import {Provider} from "react-redux";
+import store from "./store.ts";
+import Game from "./modules/game/Game.tsx";
 
 let rootElement = document.getElementById("root")
 if (!rootElement) throw new Error('Root not found');
 
 const root = createRoot(rootElement)
 
-function Game() {
-    const gameState = useSelector((globalState: RootState) => globalState.game);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            // Compute scoreupdate and update score once accordingly
-            let scoreUpdate = 0;
-            gameState.ownedItems.forEach((item) => {
-                scoreUpdate += item.linesPerMilliseconds;
-            });
-
-            dispatchIncrementScore(scoreUpdate)
-        }, 100);
-
-        return () => clearInterval(interval);
-    }, [gameState]);
-
-    const dispatchBuyItem = (item: Item) => {
-        dispatch(buyItem(item));
-    }
-
-    const dispatchIncrementScore = (incr = 1) => {
-        dispatch(incrementScore(incr));
-    }
-
-    return (
-        <div>
-            <div>
-                <h2>You generated {Math.round(gameState.currentScore)} lines of code</h2>
-            </div>
-            <h3>Your items: </h3>
-            <ul>
-                {gameState.ownedItems.map((oi: Item, index) => (
-                    <li key={index}>{oi.name}</li>
-                ))}
-            </ul>
-            <div>
-
-            </div>
-            <button onClick={() => dispatchIncrementScore()}>Code !</button>
-            <div>
-                {items.map((item) => (
-                        <div key={item.name}>
-                            {item.name} {item.price}
-                            <button
-                                disabled={item.price > gameState.currentScore}
-                                onClick={() => dispatchBuyItem(item)}
-                            >
-                                {item.name}
-                            </button>
-                        </div>
-                    )
-                )}
-            </div>
-        </div>
-    )
-}
-
-const element = (
+const App = () => (
     <>
         <Provider store={store}>
             <h1>Welcome to git-clicker game</h1>
@@ -79,4 +18,4 @@ const element = (
     </>
 );
 
-root.render(element);
+root.render(<App />);
