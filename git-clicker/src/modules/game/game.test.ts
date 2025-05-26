7f66b69ee6065f2b85018343cc3c2913510c9b59
Damien PIQUET <piqudam@gmail.com>
2025-05-26 10:21:36 +0200

    added some tests

---
+++ git-clicker/src/modules/game/game.test.ts
@@ -0,0 +1,42 @@
+import game, {buyItem, incrementScore, Item} from './index.ts'
+
+describe('game reducer', () => {
+    it('should handle incrementScore', () => {
+        const initialState = {
+            ownedItems: [] as Item[],
+            currentScore: 30,
+        }
+
+        const incrementAction = incrementScore(1)
+        const actual = game(initialState, incrementAction)
+        const expected = {
+            ownedItems: [] as Item[],
+            currentScore: 31,
+        }
+
+        expect(actual).toEqual(expected)
+    })
+
+    it('should handle buyItem', () => {
+        const initialState = {
+            ownedItems: [] as Item[],
+            currentScore: 30,
+        }
+
+        const item = {
+            name: 'test',
+            price: 10,
+            linesPerMilliseconds: 1000,
+        }
+
+        const buyItemAction = buyItem(item)
+
+        const actual = game(initialState, buyItemAction)
+        const expected = {
+            ownedItems: [item],
+            currentScore: 20,
+        }
+
+        expect(actual).toEqual(expected)
+    })
+})
